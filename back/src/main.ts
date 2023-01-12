787c869b4534d32e80631e320eceff4fbce9c01c
Yaroslav Makarov <slideryo@gmail.com>
2023-01-12 23:06:19 +0500

    back init done

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
+import { Swagger } from './conf/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('/api');
+  Swagger(app);
+  app.enableCors({
+    optionsSuccessStatus: 200,
+    origin: JSON.parse(process.env.CORS_ORIGIN),
+  });
+
+  await app.listen(process.env.PORT, () =>
+    console.log(`Server started on port ${process.env.PORT}`),
+  );
 }
 bootstrap();
